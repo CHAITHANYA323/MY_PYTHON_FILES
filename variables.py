57a80effdee453a1d633927ef18ded9d9c9c2341
CHAITHANYA REDDY <kasa.chaithanyareddy@gmail.com>
2026-06-30 15:51:14 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,132 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#variables
+print(3+8)
+11
+a=10
+print(a)
+10
+b=20
+print(b)
+20
+x=40
+print(X)
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    print(X)
+NameError: name 'X' is not defined. Did you mean: 'x'?
+print(x)
+40
+z=50
+pront(z)
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    pront(z)
+NameError: name 'pront' is not defined. Did you mean: 'print'?
+print(z)
+50
+3=80
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+a3=80
+print(a3)
+80
+
+6a=90
+SyntaxError: invalid decimal literal
+b1234567=300
+print(b123456789)
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    print(b123456789)
+NameError: name 'b123456789' is not defined. Did you mean: 'b1234567'?
+@=60
+SyntaxError: invalid syntax
+print(@)
+SyntaxError: invalid syntax
+_=50
+print(_)
+50
+ =90
+ 
+SyntaxError: unexpected indent
+_=100
+print(_)
+100
+b1234567=300
+print(b1234567)
+300
+if=20
+SyntaxError: invalid syntax
+a=4,b=9
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+a=4;b=9
+print(a+b)
+13
+a,b=3,4
+print(a,b)
+3 4
+a=2,3,4
+print(a)
+(2, 3, 4)
+a,b,c=10
+Traceback (most recent call last):
+  File "<pyshell#35>", line 1, in <module>
+    a,b,c=10
+TypeError: cannot unpack non-iterable int object
+a=b=c=10
+print(a,b,c)
+10 10 10
+a,b,c=(2,3,4)
+print(a,b,c)
+2 3 4
+a,b,c-2,3,4,5,6,7,8,9
+(2, 3, 2, 3, 4, 5, 6, 7, 8, 9)
+a,b,c,=2,3,4,5,6,7
+Traceback (most recent call last):
+  File "<pyshell#41>", line 1, in <module>
+    a,b,c,=2,3,4,5,6,7
+ValueError: too many values to unpack (expected 3, got 6)
+>>> first name="chaithu"
+SyntaxError: invalid syntax
+>>> first_name="pooja"
+>>> print(first_name)
+pooja
+>>> firstname="pooja"
+>>> print(firstname)
+pooja
+>>> fname="chaithu"
+>>> lname="ka"
+>>> print(fname+lname)
+chaithuka
+>>> print(fname+" "+lname)
+chaithu ka
+>>> print(fname,lname)
+chaithu ka
+>>> name="chaithu"
+>>> print(name)
+chaithu
+>>> print("name")
+name
+>>> city="vja"
+>>> print(city)
+vja
+>>> a=5
+>>> print(a)
+5
+>>> del a
+>>> print(a)
+Traceback (most recent call last):
+  File "<pyshell#60>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined. Did you mean: 'a3'?
+>>> name='chaithu"
+SyntaxError: unterminated string literal (detected at line 1)
+>>> name="chaithu"
+>>> print(name)
+chaithu
+>>> NAME="chaithu"
+>>> print(NAME)
+chaithu
+>>> Name="chaithu"
+>>> print(Name)
+chaithu
