15e1b57fd7529e32e61c2f228e950ca158531574
CHAITHANYA REDDY <kasa.chaithanyareddy@gmail.com>
2026-07-07 20:04:31 +0530

    Add files via upload

---
+++ condtitions (if) day1.py
@@ -0,0 +1,223 @@
+#conditions
+#if-condition by using comparsion operators
+#<,>,<=,>=,!=,==
+'''a=2
+b=4
+if a<b:
+    print("true")'''
+
+'''a=20
+b=40
+if b>a:
+    print("true")'''
+
+'''a=12
+b=14
+if a<=b:
+    print("true")'''
+
+'''a=7
+b=9
+if a!=b:
+    print("not equal")'''
+
+'''a=5
+b=10
+if a==b:
+    print("not equal")'''
+
+'''a=5
+b=10
+if a==b:
+    print("equal")'''
+
+'''a=8
+b=8
+if a==b:
+    print("true")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<b:
+    print("less")'''
+
+
+'''a=int(input("a value"))
+if a<10:
+    print("less")'''
+
+'''a="python"
+if a=="java"
+print("true")'''
+
+'''a=input("data")
+if a=="java":
+    print("true")'''
+
+
+#if-condition by using logical operators
+#and ,or,not
+
+#and
+'''a=4
+b=9
+if a,b and b>a:
+    print("less")'''
+
+'''a=4
+b=9
+if a<=b and b>=a:
+    print("less")'''
+
+'''a=4
+b=9
+if a!b and a==b:
+        print("true")'''
+
+#or
+
+'''a=7
+b=11
+if a<b or b>a:
+    print("less")'''
+
+'''a=6
+b=8
+if a<=b or b>=a:
+    print("less")'''
+
+'''a=4
+b=9
+if a!b or a==b:
+    print("less")'''
+
+'''a=13
+b=15
+if not a<b:
+    print("less")'''
+
+'''a=13
+b=15
+if not a>b:
+    print("less")'''
+
+'''a=13
+b=15
+if not a<b and b>a:
+    print("less")'''
+
+
+'''a=13
+b=15
+if not a<b or b>a:
+    print("less")'''
+
+    
+'''a=int (input())
+b=int (input())
+if a<b and b>a:
+    print("less")'''
+
+
+# if-condition by using operators
+#is,is not
+'''a=5
+if type(a) is int:
+    print("it is int")
+
+a=6
+if type(a) is not init:
+    print("false")
+
+a=int (input("a value"))
+if type (a) is int:
+    print( "it is int") '''
+
+'''a="python"
+if type (a) is str:
+    print("it is str")'''
+
+'''a=(input("a value"))
+if type (a) is str:
+    print( "it is str")'''
+
+'''a=5.8
+if type(a) is float:
+    print("it is float")'''
+
+'''a=float (input("a value"))
+if type (a) is float:
+    print( "it is float")'''
+
+'''a=6
+if type(a) is  float:
+    print("true")'''
+
+
+#if-condition by using membership operators
+#in ,not in
+'''a=2,3,4,5,6,7,8,9,10
+if 10 in a:
+    print("true")'''
+
+'''a=2,3,4,5,6,7,8,9,10
+if 10 in a:
+    print("true")'''
+
+'''a=2,3,4,5,6,7,8,9,10
+if 20 in a:
+    print("true")'''
+    
+'''a=int(input("enter the value"))
+if 30 not in a:
+    print("true")'''#error
+
+'''a=2,3,4,5,6,7,8,9,10
+b=int(input("a value"))
+if b in a:
+    print("true")'''
+
+
+
+
+
+    
+
+    
+
+    
+
+
+    
+
+    
+
+
+
+
+    
+
+    
+    
+
+
+    
+    
+
+
+    
+
+
+    
+    
+        
+
+    
+
+    
+
+    
+    
+  
+
+     
