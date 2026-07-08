497cd39e206cb8ba5bf4e6a63132bb25b41bdc3f
CHAITHANYA REDDY <kasa.chaithanyareddy@gmail.com>
2026-07-08 20:06:29 +0530

    Add files via upload

---
+++ conditions (nested-if) day2.py
@@ -0,0 +1,80 @@
+#By using nested-if
+
+'''a=4
+b=6
+if a>b:
+    print("less")
+    if b>a:
+        print("greater")'''
+
+'''a=4
+b=6
+if a>b:
+    print("less")
+    if b>a:
+        print("greater")'''
+
+a=7
+b=12
+if a<b:
+    print("less")
+    if b==a:
+        print("greater")
+
+a=12
+b=13
+if a>b:
+    print("less")
+    if b==a:
+        print("greater")
+    else:
+        print("true")
+
+a=15
+b=17
+if a>b:
+    print("less")
+    if b>a:
+        print("greater")
+else:
+    print("true")
+
+
+a=9
+b=11
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")
+    else:
+        print("true")
+else:
+    print("false")
+
+a=4
+b=6
+if a<b:
+    print("less")
+    if b==a:
+        print("greater")
+        elif a!=b:
+            print("not equal")
+
+ a=4
+ b=6
+ if a<b:
+     print("less")
+     if b==a:
+         print("greater")
+     elif a>=b:
+         print("not equal")
+     else:
+         print("equal")
+             
+        
+        
+
+
+        
+        
+        
