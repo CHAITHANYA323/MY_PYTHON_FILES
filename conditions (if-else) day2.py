df6cd4c26b6790540c75efaedb0fa22918f65335
CHAITHANYA REDDY <kasa.chaithanyareddy@gmail.com>
2026-07-08 20:05:03 +0530

    Add files via upload

---
+++ conditions (if-else) day2.py
@@ -0,0 +1,73 @@
+#if-else conditions by using comparision operators
+
+'''a=3
+b=6
+if a>b:
+    print("true")
+else:
+    print("false")
+
+    
+a=3
+b=6
+if a>b:
+    print("true")
+else:
+    print("false")
+    
+a=9
+b=11
+if a>!=b:
+    print("not equal")
+else:
+    print("true")'''
+
+
+#if-else condition by using logical operators
+
+'''a=5
+b=10
+if a<b and b>a:
+    print("less")
+else :
+    print("true")'''
+
+a=5
+b=10
+if a<b or b>a:
+    print("true")
+else:
+    print("flase")
+
+#if-else condition by using identify operators
+a=4
+if type(a) is int:
+    print("it is int")
+else:
+    print("false")
+
+ a=5
+ if type(a) is not int:
+     print("it is int")
+else:
+    print("it is not int")
+    
+#if-else condition by using membership operators
+a=1,2,3,4,5,6
+ if 2 in a:
+     print("true")
+ else:
+     print("false")
+
+ if 10 in a:
+     print("true")
+ else:
+     print("false")
+
+if 4 not in a:
+    print("true")
+else:
+    print('false")
+
+
+    
