cf32b7d9c8a971d0770f34007c3012d89178ac61
CHAITHANYA REDDY <kasa.chaithanyareddy@gmail.com>
2026-07-09 22:17:15 +0530

    Add files via upload

---
+++ conditions (TASK).py
@@ -0,0 +1,169 @@
+# Task-1
+#voting
+voting >=18 ->eligible for vote
+<18 not eligible for vote
+
+age=int(input("enter the age"))
+if age>=18:
+    print("eligible for vote")
+else:
+    print(" not eligbile for vote")
+
+
+#Task-2
+#even or odd    
+num=int(input("enter the number"))
+if num%2==0:
+     print("it is even")
+else:
+    print("it is odd")
+    
+
+#Task-3
+#leap year    
+year=int(input("enter a year"))
+if year%4==0:
+    print("it is a leap year")
+else:
+    print("it is not a leap)
+    
+
+#Task-4
+#Guest code
+          
+name=input("enter name:")
+if name== "chaithu":
+          print("welcome chaithu")
+else:
+    print("welcome guest")
+    
+    
+name=input("enter name:").lower()
+if name== "chaithu":
+          print("welcome chaithu")
+else:
+    print("welcome guest")
+
+
+#Task-5
+#guest code
+# 5 numbers
+names=["chaithu","raju","sai","srinu","tej"]
+a=input("enter the name").lower()
+if a in names:
+    print("welcome",a)
+else:
+    print("welcome guest")
+
+#Task-6
+#vowels
+vowels=["a","e","i","o","u"]
+a=input("enter the letter")
+if ain vowels:
+    print("it is vowel")
+else:
+    print("it is consonant")
+
+
+    
+
+#nested if Tasks
+
+#Task-1
+
+#social-media-login
+
+login=input("enter username")
+password=input("enter the password")
+if name=="chaithu":
+    ifPassword==1234:
+        print("login successfully")    
+    
+else:
+    print("invalid credentials")
+    
+    
+ 
+
+login=input("enter username")
+password=input("enter the password")
+if name=="chaithu":
+    if Password==1234:
+        print("login successfully")
+    else:
+        print("incorrect password")
+            
+else:
+    print("invalid username")
+
+
+
+#by using if-else method
+
+
+login=input("enter username")
+password=int(input("enter the password"))
+if name=="chaithu" and Password==1234:
+        print("login successfully")            
+else:
+    print("invalid credentials")
+
+
+login=input("enter username")
+password=input("enter the password")
+if name=="chaithu" and Password==chaithu@1234
+        print("login successfully")            
+else:
+    print("invalid credentails")     
+ 
+
+#multiple-if Tasks
+
+age=int(input("enter the age"))
+marks=int(input("enter the marks")
+attendance=int(input("enter the attendance"))
+if age>=18:
+    print("eligible for vote")
+if marks>=18:
+    print("eligible for scholarship")
+if attendance>=70:
+    print("allow to write exams")
+
+          
+
+
+
+age=int(input("enter the age"))
+marks=int(input("enter the marks")
+attendance=int(input("enter the attendance"))
+if age>=18:
+    print("eligible for vote")
+else:
+    print("not eligible for vote")
+          
+if marks>=18:
+    print("eligible for scholarship")
+
+else:
+    print("not eligible for scholarship")
+    
+if attendance>=70:
+    print("allow to write exams")
+    
+else:
+    print("not eligible for write exams")          
+
+
+
+
+    
+          
+          
+          
+    
+    
+    
+    
+              
+
+     
