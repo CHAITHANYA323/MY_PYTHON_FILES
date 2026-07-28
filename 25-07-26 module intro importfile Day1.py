6ee6ab9859229cf4fe3a3f7fc91582cccf1e805b
CHAITHANYA REDDY <kasa.chaithanyareddy@gmail.com>
2026-07-28 11:27:40 +0530

    Add files via upload

---
+++ 25-07-26 module intro importfile Day1.py
@@ -0,0 +1,19 @@
+import mymodule
+mymodule.greetings("sai")
+
+import mymodule
+
+import mymodule
+
+'''import mymodule
+a=mymodule.details["name"]
+b=mymodule.details["idnos"]
+c=mymodule.details["marks"]
+a=mymodule.details["name","idnos"] #-->  #ERROR
+print(a)#--> The will be print line by line
+print(b)
+print(c)
+print(a,b)'''# --> The will be print side by side
+
+
+#import module --> o/p--this  program  is run as module
